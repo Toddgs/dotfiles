022b16810021f2f1f757bacaf9508e5c84faca8a
Todd <toddscroggins@gmail.com>
2023-09-05 01:54:26 -0500

    Switch to lazy.nvim since packer.nvim deprecated

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,7 +12,22 @@ require('keys')      -- Keymaps
 require('plug')      -- Plugins
 require('opts')      -- Options
 
+-- Lazy.vim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- PLUGINS
+require("lazy").setup("plug")
 require('nvim-tree').setup{}
 require('lualine').setup{}
 require('nvim-autopairs').setup{}
--- dot_config/nvim/lua/plug.lua
+++ dot_config/nvim/lua/plug.lua
@@ -1,89 +1,89 @@
 -- [[ plug.lua ]]
 
-return require('packer').startup(function(use)
-	-- [[ Plugins Go Here ]]
-	-- Tree browser, similar to nerdtree
-	use {
-		'kyazdani42/nvim-tree.lua',
-		requires = 'kyazdani42/nvim-web-devicons'
-	}
+return {
+    -- [[ Plugins Go Here ]]
+    -- Tree browser, similar to nerdtree
+    {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    },
 
-	-- THEMES
-	use { 'mhinz/vim-startify' }
-	use { 'DanilaMihailov/beacon.nvim' }
-	use { 'kyazdani42/nvim-web-devicons' }
-    use { 'Mofiqul/dracula.nvim' }
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = {'kyazdani42/nvim-web-devicons',
-					opt = true}
-	}
-	use({
-		"catppuccin/nvim", as = "catppuccin",
-		config = function()
-			vim.cmd('colorscheme catppuccin')
-			require("catppuccin").setup {
-				color_overrides = {
-				}
-			}
-		end
-	})
-	use { 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup {} end }
-	use({ "ziontee113/color-picker.nvim",
-		config = function()
-			require("color-picker")
-		end,
-	})
+    -- THEMES
+    { 'mhinz/vim-startify' },
+    { 'DanilaMihailov/beacon.nvim' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'Mofiqul/dracula.nvim' },
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons',
+                    opt = true}
+    },
+    {
+        "catppuccin/nvim", as = "catppuccin",
+        config = function()
+            vim.cmd('colorscheme catppuccin')
+            require("catppuccin").setup {
+                color_overrides = {
+                }
+            }
+        end
+    },
+    { 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup {} end },
+    { "ziontee113/color-picker.nvim",
+        config = function()
+            require("color-picker")
+        end,
+    },
 
-	-- DEV
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-    use {
-        'winston0410/range-highlight.nvim',
-        requires = { 'winston0410/cmd-parser.nvim' }
-    }
-	use {
-	   "m4xshen/hardtime.nvim",
-	   requires = { 'MunifTanjim/nui.nvim', "nvim-lua/plenary.nvim" }
-	}
-	use { 'majutsushi/tagbar' }
-	use { 'Yggdroot/indentLine' }
-	use { 'tpope/vim-fugitive' }
-	use { 'junegunn/gv.vim' }
-	use { 'windwp/nvim-autopairs' }
-	use {
-		'kdheepak/tabline.nvim',
-		config = function()
-			require 'tabline'.setup {
-				-- Defaults configuration options
-				enable = true,
-				options = {
-					-- If lualine is installed tabline will use separators configured in lualine by default.
-					-- These options can be used to override those settings.
-					component_separators = { '', '' },
-					section_separators = { '', '' },
-					max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-					show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
-					show_devicons = true, -- this shows devicons in buffer section
-					colored = true,
-					show_bufnr = false, -- this appends [bufnr] to buffer section,
-					tabline_show_last_separator = true,
-					show_filename_only = true, -- shows base filename only instead of relative path in filename
-					modified_icon = "+ ", -- change the default modified icon
-					modified_italic = true, -- set to true by default; this determines whether the filename turns italic if modified
-					show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
-				}
-			}
-			vim.cmd [[
+    -- DEV
+    {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    },
+    {
+    'winston0410/range-highlight.nvim',
+    requires = { 'winston0410/cmd-parser.nvim' }
+    },
+    {
+       "m4xshen/hardtime.nvim",
+       requires = { 'MunifTanjim/nui.nvim', "nvim-lua/plenary.nvim" }
+    },
+    { 'majutsushi/tagbar' },
+    { 'Yggdroot/indentLine' },
+    { 'tpope/vim-fugitive' },
+    { 'junegunn/gv.vim' },
+    { 'windwp/nvim-autopairs' },
+    {
+        'kdheepak/tabline.nvim',
+        config = function()
+            require 'tabline'.setup {
+                -- Defaults configuration options
+                enable = true,
+                options = {
+                    -- If lualine is installed tabline will separators configured in lualine by default.
+                    -- These options can be used to override those settings.
+                    component_separators = { '', '' },
+                    section_separators = { '', '' },
+                    max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+                    show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
+                    show_devicons = true, -- this shows devicons in buffer section
+                    colored = true,
+                    show_bufnr = false, -- this appends [bufnr] to buffer section,
+                    tabline_show_last_separator = true,
+                    show_filename_only = true, -- shows base filename only instead of relative path in filename
+                    modified_icon = "+ ", -- change the default modified icon
+                    modified_italic = true, -- set to true by default; this determines whether the filename turns italic if modified
+                    show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+                }
+            }
+            vim.cmd [[
       set guioptions-=e " Use showtabline in gui vim
       set sessionoptions+=tabpages,globals " store tabpages and globals in session
     ]]
-		end,
-		requires = { { 'hoob3rt/lualine.nvim', opt = true }, { 'kyazdani42/nvim-web-devicons', opt = true } }
-	}
-	-- Autocomplete
-	use {'neoclide/coc.nvim', branch = 'release'}
+        end,
+        requires = { { 'hoob3rt/lualine.nvim', opt = true }, { 'kyazdani42/nvim-web-devicons', opt = true } }
+    },
+    -- Autocomplete
+    {'neoclide/coc.nvim', branch = 'release'}
 
-end)
+}
