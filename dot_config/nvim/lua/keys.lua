1e59146dd38af7b23248e9968e17811ea8b85032
Todd <toddscroggins@gmail.com>
2023-09-27 11:41:53 -0500

    Updating keybinds for bspwm and nvim

--- dot_config/nvim/lua/keys.lua
+++ dot_config/nvim/lua/keys.lua
@@ -1,8 +1,13 @@
 --[[ keys.lua ]]
 local map = vim.api.nvim_set_keymap
+local ts_builtin = require('telescope.builtin')
 
 -- Keybinds for plugins
 map('n', '<leader>n', [[:NvimTreeToggle]], {})
 map('n', '<leader>l', [[:IndentLinesToggle]], {})
 map('n', '<leader>t', [[:TagbarToggle]], {})
-map('n', '<leader>ff', [[:Telescope find_files]], {})
+vim.keymap.set('n', '<leader>ff', ts_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', ts_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', ts_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', ts_builtin.help_tags, {})
+
